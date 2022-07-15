999c37714bebdffd959333688e2edd9d3f58ab1e
MeetDarkPow <meet1708@gmail.com>
2022-07-16 01:31:52 +0530

    Reactables for completed package translation

--- global_translations.R
+++ global_translations.R
@@ -1,4 +1,4 @@
-# libraries
+# library
 library(tidyverse)
 
 # Reading "message_status.csv" and "metadata.csv"
@@ -26,31 +26,11 @@ final_df <- final_df %>%
   mutate(translated_count = coalesce(translated_count, 0), untranslated_count = coalesce(untranslated_count, 0))
 final_df <- subset(final_df, select = -c(translated.x, translated.y))
 
+# Fully Translated Packages with respective languages associated
 
+complete_trans_df <- subset(final_df, untranslated_count == 0)
 
-# kktt <- subset(final_df, language=="Spanish",)
-# 
-# fig <- plot_ly(kktt, x = ~package, y = ~translated_count, type = 'bar', name = 'Translated Messages')
-# fig <- fig %>% add_trace(y = ~untranslated_count, name = 'Untranslated Messages')
-# fig <- fig %>% layout(yaxis = list(title = 'Count'), barmode = 'group')
+# Untranslated Packages with respective languages associated
 
+complete_untrans_df <- subset(final_df, translated_count == 0)
 
-
-# 
-# create_chart <- function(data, var){
-#   
-#   data %>%
-#     filter(package == var) %>%
-#     e_chart(x = package) %>%
-#     e_bar(serie = translated_count, name = "Translated") %>%
-#     e_bar(serie = untranslated_count, name = "Untranslated") %>%
-#     e_tooltip()
-# }
-# 
-# prepare_grid_chart <- function(data){
-#   pkg <- unique(final_df$package)
-#   for(i in 1:length(pkg)){
-#     assign(paste0("e", i), create_chart(data, pkg[i]))
-#   }
-#   e_arrange(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, rows = 4, cols = 4, title = "Translated and Untranslated R Messages")
-# }
